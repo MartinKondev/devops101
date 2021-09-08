55ccfdc22b2876b1c6ce75bfa3c13cdd2aa7b185
Martin Kondev <Martin.Kondev@ascentsoftware.eu>
2021-09-08 13:26:43 +0300

    asdf

--- WeatherForecast/Startup.cs
+++ WeatherForecast/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebApplication1
 {
@@ -47,6 +40,7 @@ namespace WebApplication1
             {
                 endpoints.MapControllers();
             });
+            ;//asdf
         }
     }
 }
